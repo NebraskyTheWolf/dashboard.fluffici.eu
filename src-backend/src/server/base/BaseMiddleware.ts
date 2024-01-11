32969819b7f51b42a286c5fa5c4326c2b5cd601e
NebraskyTheWolf <matazox302@gmail.com>
2024-01-11 23:22:01 +0100

    update(*): removing some useless files.

--- src-backend/src/server/base/BaseMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export interface CustomRequest extends Request {
-    token?: string
-}
-
-export default abstract class BaseMiddleware {
-    public abstract handle(request: CustomRequest, response: Response, next: NextFunction): void
-}
--- src-backend/src/server/routes/api.routes.ts
+++
@@ -1,8 +0,0 @@
-import BaseRoute from "@riniya.ts/server/base/BaseRoute";
-import apiController from "@riniya.ts/server/controllers/api.controller";
-
-export default class APIRoutes extends BaseRoute {
-    public register(): void {        
-        
-    }
-}
