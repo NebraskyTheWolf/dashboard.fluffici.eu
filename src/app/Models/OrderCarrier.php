22bb3b7ad9c19264e82ea0e07a040228c6d0f2b4
NebraskyTheWolf <farfy.dev@gmail.com>
2024-01-22 17:47:47 +0100

    chore(*): Fixing GenerateMonthlyReport to the new datastore

--- src/app/Models/OrderCarrier.php
+++ src/app/Models/OrderCarrier.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Metrics\Chartable;
 
 class OrderCarrier extends Model
 {
-    use HasFactory;
+    use HasFactory, Chartable;
+
+    public $table = 'order_carrier';
 }
