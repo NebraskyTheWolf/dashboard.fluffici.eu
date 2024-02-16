55520d124a250a4860f9d8671557c180f44a8b78
NebraskyTheWolf <farfy.dev@gmail.com>
2024-02-16 19:34:44 +0100

    PATCH: Add AsSource trait to DeviceAuthorization model
    Incorporated the AsSource trait from Orchid Screen package to DeviceAuthorization model class. This enhancement is intended to make more properties and methods available for usage in the context of screens, and would allow for more extensive interaction with model data from within Orchid Screen objects.

--- src/app/Models/DeviceAuthorization.php
+++ src/app/Models/DeviceAuthorization.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
 
 class DeviceAuthorization extends Model
 {
+    use AsSource;
+
     public $table = 'device_authorization';
 
     public $fillable = [
