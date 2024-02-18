edea3b0b5d487927386a6336d409fe09b98a0eb2
NebraskyTheWolf <farfy.dev@gmail.com>
2024-02-18 12:33:22 +0100

    PATCH: Update CSS and JS in Fluffici vendor files
    This commit introduces changes to the dashy.css and dashy.js files within the Fluffici vendor directory. In the CSS file, the changes include additional styling for various elements and inclusion of Bootstrap v5.2.3. As for the JS file, there seem to be changes regarding the animation interactions for the UI elements.

--- src/app/Orchid/Layouts/User/UserEditLayout.php
+++ src/app/Orchid/Layouts/User/UserEditLayout.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Orchid\Screen\Field;
 use Orchid\Screen\Fields\Cropper;
 use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Picture;
 use Orchid\Screen\Layouts\Rows;
 
 
@@ -19,6 +20,9 @@ class UserEditLayout extends Rows
      * @return Field[]
      */
     public function fields(): array {
+
+        $user = $this->query->get('user');
+
         return [
             Input::make('user.name')
                 ->type('text')
@@ -33,15 +37,25 @@ class UserEditLayout extends Rows
                 ->title(__('user.screen.user.email'))
                 ->placeholder(__('user.screen.user.email.placeholder')),
 
+            Picture::make('user.avatar')
+                ->title("User avatar")
+                ->url('https://autumn.fluffici.eu/avatars/' . $user->avatar_id)
+                ->canSee($user->avatar == 1),
+
             Cropper::make('user.avatar')
                 ->userId(Auth::id())
                 ->remoteTag('avatars')
-                ->objectId(Auth::user()->avatar_id)
                 ->minWidth(250)
                 ->maxWidth(512)
                 ->minHeight(250)
                 ->maxHeight(512)
                 ->maxFileSize(20)
+                ->canSee($this->check($user)),
         ];
+    }
+
+    public function check($user): bool
+    {
+        return (Auth::id() == $user->id);
     }
 }
