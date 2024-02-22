1c34ce3cf0d0adc23d419caccf1e0194790c46dc
NebraskyTheWolf <farfy.dev@gmail.com>
2024-02-22 14:11:16 +0100

    Add belongsTo relationship in OrderedProduct model
    Introduced a belongsTo relationship in the OrderedProduct model, linking it to the ShopProducts class. This addition will facilitate easier retrieval of the associated ShopProducts instances and generally enhance the model's capabilities in the context of manipulating data related to ordered products in the shop.

--- src/app/Models/OrderedProduct.php
+++ src/app/Models/OrderedProduct.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Orchid\Metrics\Chartable;
 
 class OrderedProduct extends Model
@@ -11,6 +12,15 @@ class OrderedProduct extends Model
     use HasFactory, Chartable;
 
     public $connection = 'shop';
-
     protected $table = "ordered_product";
+
+    /**
+     * Retrieve the associated ShopProducts model for this instance.
+     *
+     * @return BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(ShopProducts::class);
+    }
 }
