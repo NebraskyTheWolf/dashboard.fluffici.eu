9d76ced0b1b1cf9bed501a607b68182803178212
NebraskyTheWolf <farfy.dev@gmail.com>
2024-02-22 15:55:03 +0100

    Add ReminderTest console command
    This commit introduces a new console command called "ReminderTest". This command creates a mock event and sends a reminder email for it based on the information provided inside the handle function. It's meant for testing reminder mail functionality, ensuring that reminders are sent correctly for events.

---
+++ src/app/Console/Commands/ReminderTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\ReminderMail;
+use App\Models\Events;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class ReminderTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reminder-test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $event = new Events();
+        $event->name = "Test";
+        $event->descriptions = "Test";
+        $event->begin = Carbon::today();
+        $event->end = Carbon::today()->addDays(30);
+        $event->status = "STARTED";
+        $event->type = "PHYSICAL";
+        $event->min = "{\"lat\": \"\", \"lng\":\"\"}";
+        $event->max = "{\"lat\": \"\", \"lng\":\"\"}";
+        $event->city = "Praha";
+        $event->link = "https://fox-around.com";
+        // No need to save.
+        // $event->save();
+
+        Mail::to('vakea@fluffici.eu')->send(new ReminderMail($event, 'Vakea'));
+
+        printf("Mail sent.");
+    }
+}
