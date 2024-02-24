1989196d5b082f90b067498a2c37d3d078983f0f
NebraskyTheWolf <farfy.dev@gmail.com>
2024-02-24 14:00:36 +0100

    Refactor service-worker.js for Beams integration
    The update simplifies the service-worker.js file to specifically focus on Beams integration. It removes the previous multi-state registration process and direct BeamClient usage, and introduces a more streamlined approach with an instance of PusherPushNotifications.Client. On notification received, a web push notification is now immediately displayed.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,34 +1,23 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
 window.navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
-    window.BeamClient.start().then(() => {
+    const beamsClient = new PusherPushNotifications.Client({
+        instanceId: '63e32cff-b20c-4c92-bb49-0e40cfd1dbe3',
+        serviceWorkerRegistration: serviceWorkerRegistration,
+    })
+
+    beamsClient.start().then(() => {
         console.log('Beams client started');
     }).catch(error => {
         console.error('Error starting Beams client:', error);
     });
 
-    window.BeamClient.getRegistrationState()
-        .then((state) => {
-            let states = PusherPushNotifications.RegistrationState;
-            switch (state) {
-                case states.PERMISSION_DENIED: {
-                    alert('Push notification permission denied.')
-                    break;
-                }
-                case states.PERMISSION_GRANTED_REGISTERED_WITH_BEAMS: {
-                    window.BeamClient.start()
-                        .then(() => beamsClient.addDeviceInterest('dashboard'))
-                        .then(() => console.log('Successfully registered and subscribed!'))
-                        .catch(console.error);
-                    break;
-                }
-                case states.PERMISSION_GRANTED_NOT_REGISTERED_WITH_BEAMS:
-                case states.PERMISSION_PROMPT_REQUIRED: {
-                    window.BeamClient.start()
-                        .then(() => beamsClient.addDeviceInterest('dashboard'))
-                        .then(() => console.log('Successfully registered and subscribed!'))
-                        .catch(console.error);
-                    break;
-                }
-            }
-        }).catch((e) => console.error("Could not get registration state", e));
+    beamsClient.onNotificationReceived = ({ pushEvent, payload }) => {
+        pushEvent.waitUntil(
+            self.registration.showNotification(payload.notification.title, {
+                body: payload.notification.body,
+                icon: payload.notification.icon,
+                data: payload.data,
+            })
+        );
+    };
 });
