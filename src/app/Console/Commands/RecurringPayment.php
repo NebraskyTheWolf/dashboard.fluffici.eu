732d39476ccd7af60fec71194772df2748c046f5
NebraskyTheWolf <farfy.dev@gmail.com>
2024-05-10 02:49:08 +0200

    FIX: Update RecurringPayment command to accurately handle monthly payments
    Modified the RecurringPayment command's logic to accurately account for recurring payments on a monthly basis. The script now specifically checks if the current day and month match the recurring payment day and month. If they do, a new payment entry is created, and the next recurring payment date is updated to the following month.

--- src/app/Console/Commands/RecurringPayment.php
+++ src/app/Console/Commands/RecurringPayment.php
@@ -26,14 +26,28 @@ class RecurringPayment extends Command
         $payments = Accounting::all();
 
         foreach ($payments as $payment) {
+            // Check if the payment is recurring
             if ($payment->is_recurring) {
-                if (Carbon::parse($payment->recurring_at)->isPast()) {
+                // Parse the date and time at which the recurring payment should be made
+                $recurringAt = Carbon::parse($payment->recurring_at);
+
+                // Check if the current date is past the recurring payment's date
+                // And the current day and month are the same as the recurring payment's day and month
+                if (Carbon::now()->greaterThanOrEqualTo($recurringAt)
+                    && Carbon::now()->day == $recurringAt->day
+                    && Carbon::now()->month == $recurringAt->month) {
+
+                    // Create and save the new recurring payment
                     $recurringPayment = new Accounting();
                     $recurringPayment->type = $payment->type;
                     $recurringPayment->source = $payment->source . ' (Recurring)';
                     $recurringPayment->amount = $payment->amount;
                     $recurringPayment->is_recurring = false;
                     $recurringPayment->save();
+
+                    // Update the next recurring payment's date
+                    $payment->recurring_at = $recurringAt->addMonth();
+                    $payment->save();
                 }
             }
         }
