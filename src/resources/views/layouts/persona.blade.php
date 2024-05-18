6c8b692051784c306a68466c653ff0657a1a968f
NebraskyTheWolf <farfy.dev@gmail.com>
2024-05-18 15:19:08 +0200

    Update style properties of persona avatar image
    In the "layouts/persona.blade.php" file, the style attributes of the image tag for persona avatar are updated to limit its height and width to 40 pixels. This ensures a consistent visual appearance and better layout alignment across various platforms.

--- src/resources/views/layouts/persona.blade.php
+++ src/resources/views/layouts/persona.blade.php
@@ -2,7 +2,7 @@
     <div class="d-sm-flex flex-row flex-wrap align-items-center">
         @empty(!$image)
             <span class="thumb-sm avatar me-sm-3 ms-md-0 me-xl-3 d-none d-md-inline-block">
-              <img src="{{ $image }}" class="bg-light" id="persona-avatar">
+              <img src="{{ $image }}" class="bg-light" id="persona-avatar" style="max-height: 40px; height: 40px; max-width: 40px; width: 40px;">
             </span>
         @endempty
         <div class="mt-2 mt-sm-0 mt-md-2 mt-xl-0">
